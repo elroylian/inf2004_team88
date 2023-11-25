7cc5c87d7ca1a12e9a3d258bb419553d0631fff0
Elroy <elroy7602@gmail.com>
2023-11-25 14:51:48 +0800

    updated ultrasonic to read 2dp

--- driver/ultrasonic/ultrasonic.h
+++ driver/ultrasonic/ultrasonic.h
@@ -1,6 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+#include "pico/time.h"
+
+const uint trigPin = 0;
+const uint echoPin = 1;
+
 #ifndef ultrasonic_h
 #define ultrasonic_h
-void setupUltrasonicPins(int trigPin, int echoPin);
-int getCm(int trigPin, int echoPin);
-int getInch(int trigPin, int echoPin);
+void setupUltrasonicPins(uint trigPin, uint echoPin);
+float getDistance(uint trigPin, uint echoPin);
 #endif
